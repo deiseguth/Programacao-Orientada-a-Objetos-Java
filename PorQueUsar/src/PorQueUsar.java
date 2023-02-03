7d2cd3d8b476d24ca26b795c2ebef5e35cc0f763
deiseguth <deiiseguth@gmail.com>
2023-02-03 20:13:46 -0300

    1; 2,5

---
+++ PorQueUsar/src/PorQueUsar.java
@@ -0,0 +1,16 @@
+/*
+    PE VS POO
+      Paradigma estruturado tem uma representação mais simplista. Foca em operaçõexs (funções) e dados. Foca mais no "como fazer"
+
+      Paradigma orientado a objeto tem uma representação mais realista. Foca na modelagem de entidades e nas interações entre estas. Foca mais no "o que fazer"
+
+
+    Vantagens da Programação Orientada a Objetos (POO)
+        Melhor coesão: funções bem definidas. 
+        Melhor acoplamento: mais flexíveis. 
+        Diminuição do Gap semântico. 
+        Coletor de lixo: liberando memória do computador. 
+    
+
+
+*/
